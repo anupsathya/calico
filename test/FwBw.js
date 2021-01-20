7ea64fee8051320aa34669a5b0455c9bf17a2152
Anup S <anup.sathya@outlook.com>
2021-01-19 21:50:58 -0500

    Forward Backward

---
+++ test/FwBw.js
@@ -0,0 +1,15 @@
+function moveForward(a) {
+  setTimeout(function () {
+  D25.write(0);
+    analogWrite(D24,0);
+}, a);
+  analogWrite(D24, 1);
+}
+
+function moveBackward(a) {
+  setTimeout(function () {
+  D24.write(0);
+    analogWrite(D25,0);
+}, a);
+  analogWrite(D25, 1);
+}
